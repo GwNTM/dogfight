f9cd4d868b7357929cddc99ba9a5d210c69d98de
km229 <kevin.moriceau@viacesi.fr>
2018-04-24 11:04:18 +0200

    Phase 3

--- src/controller/DogfightController.java
+++ src/controller/DogfightController.java
@@ -1,5 +1,31 @@
 package controller;
 
-public class DogfightController {
-
+public class DogfightController implements IOrderPerformer{
+	private static int TIME_SLEEP =30;
+	private IDogfightModel dogfightModel;
+	private IViewSystem viewSystem;
+	
+	public DogfightController(IDogfightModel dogfightModel){
+		
+	}
+	
+	public void orderPerform(UserOrder userOrder){
+		
+	}
+	
+	public void play(){
+		
+	}
+	
+	public void setViewSystem(IViewSystem viewSstem){
+		
+	}
+	
+	private void lauchMissile(int player){
+		Missile missile = new Missile();
+	}
+	
+	private void gameLoop(){
+		
+	}
 }
--- src/controller/UserOrder.java
+++ src/controller/UserOrder.java
@@ -1,5 +1,19 @@
 package controller;
 
-public class UserOrder {
+public class UserOrder implements IUserOrder{
+	private int player;
+	private Order order;
+	
+	public UserOrder(int player, Order order){
+		
+	}
 
+	public Order getOrder() {
+		return order;
+	}
+
+	public int getPlayer() {
+		return player;
+	}
+	
 }
